15b49eab769931f1d3ac4614645a982c439e19aa
oxRahulxo <rahulqatester@hotmail.com>
2026-08-17 20:06:32 -0400

    Update Code

--- tests/linkedin course/auth.setup.ts
+++ tests/linkedin course/auth.setup.ts
@@ -5,7 +5,9 @@ setup('Create customer02 auth', async ({ page, context }) => {
   const password = 'welcome01';
   const customer02AuthFile = '.auth/customer02.json';
 
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
+  await page.goto('https://practicesoftwaretesting.com/');
+  await page.waitForLoadState('networkidle');
+  await page.getByText("Sign in").click();
   await page.getByPlaceholder('Your email').fill(email);
   await page.getByPlaceholder('Your password').fill(password);
   await page.getByRole('button', { name: 'Login' }).click();
